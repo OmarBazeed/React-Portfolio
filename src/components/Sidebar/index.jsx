e59fc073d6cced962db26c28c7329aeae45084ec
Omar Bazeed <omarbazeed@gmail.com>
2022-12-16 23:45:53 +0200

    Initial Commit And Destructuring The Project And Building The Sidebar With The Nav List Using The Nested Route With Outlet Techenique

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import './sidebar.scss';
+import Logo1 from '../../assets/images/WORK.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHomeAlt, faUserAlt } from '@fortawesome/free-solid-svg-icons';
+import { faGithubAlt, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
+const Sidebar = () => {
+  return (
+    <React.Fragment>
+
+      <section  className='sidebar'>
+
+        <NavLink to='/' className='sidebar-logo'>
+        <img src={Logo1} alt='logo1' className='logo1' />
+        <h3> Omar Bazeed</h3>
+        </NavLink>
+        
+        <ul className='sidebar-navi'>
+          <NavLink to='home' data-content='Home'>
+          <FontAwesomeIcon icon={faHomeAlt} /></NavLink>
+          <NavLink to='about' data-content='About'>
+          <FontAwesomeIcon icon={faUserAlt} /></NavLink>
+          <NavLink to='skills' data-content='Sills'>
+          <FontAwesomeIcon icon={faEnvelope} /></NavLink>
+        </ul>
+
+        <ul className='connect'>
+        <li> 
+          <a href='https://github.com/OmarBazeed' target='_blank' rel='noreferrer' > 
+          <FontAwesomeIcon icon={faGithubAlt} />
+          </a> 
+        </li>
+        <li> 
+          <a href='https://www.linkedin.com/in/omar-bazeed-516437246/'  target='_blank' rel='noreferrer'>
+          <FontAwesomeIcon icon={faLinkedinIn} /></a> 
+        </li>
+        </ul>
+
+      </section>
+    
+    </React.Fragment>
+  )
+}
+
+export default Sidebar
---
+++ src/components/Skills/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Skills = ()=>{
+
+    return(
+        <React.Fragment>
+            Skills
+        </React.Fragment>
+    )
+}
+
+export default  Skills;
