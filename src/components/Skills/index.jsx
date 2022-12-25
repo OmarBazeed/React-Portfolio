f575e1ab14724ff2e509825282cd208e72659df4
Omar Bazeed <omarbazeed@gmail.com>
2022-12-25 04:15:43 +0200

    Adding Skills Spheric Random  Words With Another Method Besides The Method Of @react-three/fiber

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,14 +1,14 @@
 import React , {useEffect, useState} from 'react';
 import AnimatedLetters from '../AnimattedLetters';
 import './skills.scss';
-import Words from './Spherical-Word-Canvas'
+import Words from './React-3D-Sphere'
 import Loader from 'react-loaders';
 
 
 const Skills = ()=>{
 
     const [letterClass , setLetterClass] = useState('text-animate');
-    const titleArray = ['M' , 'y', ' ' ,'S', 'k' , 'i' , 'l' , 'l' , 's'];
+    const titleArray = ['M' , 'y', ' ' ,'S', 'k' , 'i' , 'l' , 'l' , 's' ,' ' ,',' ,' ', 'E' , 'x' , 'p' , 'r' , 'i' , 'e' , 'n','c','e'];
 
     useEffect(()=>{
         setTimeout(()=>{
@@ -23,7 +23,7 @@ const Skills = ()=>{
 
             <div className='Skills-content'>
                 <h1 data-after="&lt;h1&gt;" data-before='&lt;/h1&gt;'>
-                    <AnimatedLetters letterClass={letterClass} strArray={titleArray} idx={15}  />
+                    <AnimatedLetters letterClass={letterClass} strArray={titleArray} idx={8}  />
                 </h1>
                 <p>
                 I Studed Web Development Track So I gained some Skills Like Html , Css , JavaScript and react  <br /> Also I Studed Graphic Design Track So I can use Photoshop , illustator and In design Besides Some Skills In Digital Merketing
