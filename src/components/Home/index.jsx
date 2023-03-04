d78aa413c57a091f37bb0ed3cfaeb8e30ceaf277
Omar Bazeed <omarbazeed@gmail.com>
2023-03-04 04:00:43 +0200

    Updating My CV

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,62 +1,82 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Developer from '../../assets/images/WORK.png';
-import './home.scss';
-import AnimatedLetters from '../AnimattedLetters';
-import Logo from './Logo';
-import Loader from 'react-loaders';
-import PDF from './Omar Bazeed.pdf';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Developer from '../../assets/images/WORK.png'
+import './home.scss'
+import AnimatedLetters from '../AnimattedLetters'
+import Logo from './Logo'
+import Loader from 'react-loaders'
+import PDF from './Omar_Bazeed_Resume_04-03-2023-03-57-11.pdf'
 
-const Home = ()=>{
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = [' ', 'O', 'm', 'a', 'r', ' ', 'B', 'a', 'z', 'e', 'e', 'd']
+  const jobArray = [
+    'W',
+    'e',
+    'b',
+    ' ',
+    'D',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
 
-    const [letterClass , setLetterClass] = useState('text-animate');
-    const nameArray = [' ','O' , 'm', 'a' , 'r', ' ', 'B' , 'a', 'z', 'e', 'e', 'd'];
-    const jobArray = ['W' , 'e', 'b' ,' ', 'D', 'e' , 'v', 'e', 'l', 'o','p', 'e','r' ,'.'];
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
 
-    useEffect(()=>{
-            setTimeout(()=>{
-            setLetterClass('text-animate-hover')
-        }, 4000) 
-    },[])
-    
+  return (
+    <React.Fragment>
+      <div className="mainHome">
+        <div className="content">
+          <h1 data-after="&lt;h1&gt;" data-before="&lt;/h1&gt;">
+            <span className={`${letterClass} _20`}>H</span>
+            <span className={`${letterClass} _20`}>i,</span>
+            <br /> <br />
+            <span className={`${letterClass} _20`}>I</span>
+            <span className={`${letterClass} _20`}>'m</span>{' '}
+            <img src={Developer} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />{' '}
+            <br /> <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
 
+          <p> Frontend Developer | Some Skills Of Graphic Designer </p>
 
-    return(
-        <React.Fragment>
-        
-            <div className='mainHome'>
+          <Link to="/contact">
+            <button> Contact Me</button>
+          </Link>
 
-                <div className='content'>
-                    <h1 data-after="&lt;h1&gt;" data-before='&lt;/h1&gt;'>
-                        <span className={`${letterClass} _20`}>H</span> 
-                        <span className={`${letterClass} _20`}>i,</span> 
-                        <br /> <br />
+          <a href={PDF} className="download" download>
+            {' '}
+            Download C.V{' '}
+          </a>
+        </div>
 
-                        <span className={`${letterClass} _20`}>I</span> 
-                        <span className={`${letterClass} _20`}>'m</span>  <img src={Developer} alt="developer" />  
-                        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}  /> <br /> <br />
-                        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22}  />
-                    </h1>
-                    
-                    <p> Frontend Developer | Some Skills Of Graphic Designer </p>
+        <div className="logo-div">
+          <Logo />
+        </div>
+      </div>
 
-                    <Link to='/contact'>
-                        <button> Contact Me</button>
-                    </Link>
-                    
-                    <a href={PDF} className='download' download > Download C.V </a> 
-                </div>
-
-                <div className='logo-div'>
-                    <Logo />
-                </div>
-
-            </div>
-
-            <Loader type='ball-spin-fade-loader' />
-
-        </React.Fragment>
-    )
+      <Loader type="ball-spin-fade-loader" />
+    </React.Fragment>
+  )
 }
 
-export default Home;
+export default Home
